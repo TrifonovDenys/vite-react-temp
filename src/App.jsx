ddd414e980606d1ad9d7b249725d860462be5f8e
Denys Tryfonov <trifonowden123@gmail.com>
2023-12-07 14:23:31 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+function App() {
+  return (
+    <>
+      <p className='text-3xl font-bold underline'>Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
